6cddb5b9cb697c62b530e89ddefdfaf9865d00ca
masrurahmadnayel <69481148+masrurahmadnayel@users.noreply.github.com>
2021-01-22 09:13:41 +0600

    starting project

---
+++ assignment.js
@@ -0,0 +1,60 @@
+// there will be a github link
+
+// first problem code:
+
+// function kilometerToMeter(km) {
+//     if (km < 0 || km.length >= 0) {      // this line of code is for handling the error values like negative or string values.
+//         console.log("Error: enter the correct value.");
+//     }
+//     else {
+//         var value = km * 1000;
+//     }
+//     return value;
+// }
+
+
+
+// second problem code:
+
+// function budgetCalculator(clock, mobile, laptop) {
+//     if (clock < 0 || mobile < 0 || laptop < 0 || clock.length >= 0 || mobile.length >= 0 || laptop.length >= 0) {
+//         console.log("Error: enter the correct value.")
+//     }
+//     else {
+//         clock = clock * 50;
+//         mobile = mobile * 100;
+//         laptop = laptop * 500;
+//         var total = clock + mobile + laptop;
+//     }
+//     return total;
+// }
+
+
+// third problem code:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fourth problem code:
